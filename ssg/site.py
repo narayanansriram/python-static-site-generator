8fae87fabd0169cd9a2b218da0a24bcb242f8522
Sriram Narayanan <sriram210@yahoo.in>
2021-10-13 15:09:06 -0400

    Parser in progress

--- ssg/site.py
+++ ssg/site.py
@@ -29,5 +29,5 @@ class Site():
         for path in self.source.rglob('*'):
             if path.is_dir():
                 self.create_dir(path)
-            elif path.is_file:
+            elif path.is_file():
                 self.run_parser(path)
